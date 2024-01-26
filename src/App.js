7f4e1de20c0899aa5948eb85da3277679b3060a0
R-holmes10 <ritutwinkle10@gmail.com>
2024-01-26 20:24:07 +0530

    Fetch URL fixed

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://127.0.0.1:5000', {
+      const response = await fetch('http://localhost:5000', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
